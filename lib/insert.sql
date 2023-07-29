c46e6e05b3fd6d35ba2af224576a66e75d972f12
Samuel <samuelou510@gmail.com>
2023-07-29 19:26:30 -0400

    sql + python is very new, need practice

---
+++ lib/insert.sql
@@ -0,0 +1,17 @@
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Mr.Chocolate", "123", "M", "123", "123", TRUE);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Rowdy", "123", "M", "123", "123", TRUE);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Rowdy", "123", "M", "123", "123", TRUE);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Rowdy", "123", "M", "123", "123", TRUE);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Rowdy", "123", "M", "123", "123", TRUE);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Rowdy", "123", "M", "123", "123", TRUE);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Rowdy", "123", "M", "123", "123", TRUE);
+-- INSERT INTO bears (age, sex, color, temperament, alive) VALUES("exit23", "M", "123", "123", TRUE);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+(2,"Rowdy", 10, "M", "black", "intense", 1),
+(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
+(4,"Sergeant Brown", 19, "M", "Green", "Slimy", 0),
+(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
+(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
+(7,"Wendy", 6, "F", "Blue", "naive", 1),
+(8,null, 20, "M", "black", "aggressive", 0);
